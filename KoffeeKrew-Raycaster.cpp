cf1c69a53091c28774e0d4dd3d4b19200627b40a
Aiden Trainer-Mollenkopf <atmollenkopf@gmail.com>
2024-11-15 19:43:09 -0500

    Add project files.

---
+++ KoffeeKrew-Raycaster.cpp
@@ -0,0 +1,12 @@
+﻿// KoffeeKrew-Raycaster.cpp : Defines the entry point for the application.
+//
+
+#include "KoffeeKrew-Raycaster.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
