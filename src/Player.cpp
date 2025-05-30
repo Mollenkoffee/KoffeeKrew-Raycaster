9e334a7a28704072ce9f9ebb45fd125675fdc219
Aiden Trainer-Mollenkopf <atmollenkopf@gmail.com>
2025-05-29 21:58:36 -0400

    Updated drawPlayer to draw an arrow rather than just a line to display player direction.

--- src/Player.cpp
+++ src/Player.cpp
@@ -39,13 +39,30 @@ void Player::rotate(float angleDelta)
     deltaY = sin(angle);
 }
 
-void Player::drawPlayer(int windowWidth, int windowHeight) const 
+void Player::drawPlayer(int windowWidth, int windowHeight) const
 {
     float scaleX = (float)windowWidth / (Map::mapXUnits * Map::mapUnitSize);
     float scaleY = (float)windowHeight / (Map::mapYUnits * Map::mapUnitSize);
 
     float playerScreenX = x * scaleX;
     float playerScreenY = y * scaleY;
+
+    // Direction vector scaled
+    float dirX = deltaX * 10 * scaleX;
+    float dirY = deltaY * 10 * scaleY;
+
+    // Arrowhead size
+    float arrowSize = 5.0f;
+
+    // Perpendicular vector for arrowhead
+    float perpX = -dirY;
+    float perpY = dirX;
+    float length = sqrt(perpX * perpX + perpY * perpY);
+    perpX = (perpX / length) * arrowSize;
+    perpY = (perpY / length) * arrowSize;
+
+    float endX = playerScreenX + dirX;
+    float endY = playerScreenY + dirY;
 
     // Draw player position
     glColor3f(1.0f, 0.0f, 0.0f);
@@ -54,9 +71,18 @@ void Player::drawPlayer(int windowWidth, int windowHeight) const
     glVertex2f(playerScreenX, playerScreenY);
     glEnd();
 
-    // Draw player direction
+    // Draw main direction line
     glBegin(GL_LINES);
     glVertex2f(playerScreenX, playerScreenY);
-    glVertex2f(playerScreenX + deltaX * 10 * scaleX, playerScreenY + deltaY * 10 * scaleY);
+    glVertex2f(endX, endY);
+    glEnd();
+
+    // Draw arrowhead
+    glBegin(GL_LINES);
+    glVertex2f(endX, endY);
+    glVertex2f(endX - dirX * 0.2f + perpX, endY - dirY * 0.2f + perpY);
+
+    glVertex2f(endX, endY);
+    glVertex2f(endX - dirX * 0.2f - perpX, endY - dirY * 0.2f - perpY);
     glEnd();
 }
